9596bbc2e6c143101c4a39ddda9e85d688fb7fd0
lovesnowex <lsktw22@gmail.com>
2023-09-08 14:05:00 +0800

    Add comment for color

--- pkg/gungi/color.go
+++ pkg/gungi/color.go
@@ -1,8 +1,9 @@
 package gungi
 
+// Color is the color of a player.
 type Color int
 
 const (
-	WHITE Color = iota
-	BLACK
+	WHITE Color = iota // First player
+	BLACK              // Second player
 )
