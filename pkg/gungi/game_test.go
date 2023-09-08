eb639797a061e95d56b2e0a1f1d786155e19429f
lovesnowex <lsktw22@gmail.com>
2023-09-08 15:14:05 +0800

    Add more test for Game

--- pkg/gungi/game_test.go
+++ pkg/gungi/game_test.go
@@ -31,6 +31,17 @@ func TestNoGamePlayers(t *testing.T) {
 	r.Nil(game.CurrentPlayer())
 }
 
+func TestGamePlayers(t *testing.T) {
+	r := require.New(t)
+	game := newTestGame(t)
+	p1 := newTestHumanPlayer(t)
+	p2 := newTestHumanPlayer(t)
+	_ = game.Join(p1, WHITE)
+	_ = game.Join(p2, BLACK)
+	r.Equal([playerCount]Player{p1, p2}, game.Players())
+	r.Equal(p1, game.CurrentPlayer())
+}
+
 func TestGameJoin(t *testing.T) {
 	r := require.New(t)
 	game := newTestGame(t)
@@ -40,4 +51,14 @@ func TestGameJoin(t *testing.T) {
 	r.NoError(game.Join(p2, BLACK))
 	r.Equal([playerCount]Player{p1, p2}, game.Players())
 	r.Equal(p1, game.CurrentPlayer())
+
+	// Test that you can't join a game with wrong color.
+	p3 := newTestHumanPlayer(t)
+	r.Error(game.Join(p3, -1))
+	r.Error(game.Join(p3, playerCount))
+
+	// Test that you can't join a game that has already started.
+	go game.Start()
+	r.Error(game.Join(p3, WHITE))
+	r.Error(game.Join(p3, BLACK))
 }
