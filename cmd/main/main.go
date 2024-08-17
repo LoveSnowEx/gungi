b132055aad924b9c337b6f546bbb0206b8ac7017
lovesnowex <lsktw22@gmail.com>
2024-08-17 16:29:03 +0800

    Remove unused GUI code and dependencies

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"github.com/LoveSnowEx/gungi/internal/bootstrap"
-	"github.com/LoveSnowEx/gungi/internal/interface/gui"
 )
 
 func main() {
 	bootstrap.SetupSlog()
 	bootstrap.SetupDB()
-
-	if err := gui.Run(); err != nil {
-		panic(err)
-	}
 }
