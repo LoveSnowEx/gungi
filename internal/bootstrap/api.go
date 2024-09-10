f6d51f0fa35607c1eb685e8440b523464efcea85
LoveSnowEx <lsktw22@gmail.com>
2024-09-10 11:39:03 +0800

    Add slog api middleware

--- internal/bootstrap/api.go
+++ internal/bootstrap/api.go
@@ -1,6 +1,10 @@
 package bootstrap
 
 import (
+	"log/slog"
+
+	"github.com/gofiber/fiber/v3/middleware/logger"
+
 	"github.com/LoveSnowEx/gungi/internal/app/user_usecase"
 	"github.com/LoveSnowEx/gungi/internal/infra/persist"
 	"github.com/LoveSnowEx/gungi/internal/interface/api/route"
@@ -9,6 +13,16 @@ import (
 )
 
 func SetupApi(app *fiber.App) {
+	app.Use(logger.New(
+		logger.ConfigDefault,
+	))
+	app.Use(func(c fiber.Ctx) (err error) {
+		err = c.Next()
+		if err != nil {
+			slog.Default().Error("api error", "err", err.Error())
+		}
+		return
+	})
 	route.Setup(app, &route.Config{
 		UserController: user_controller.New(
 			user_usecase.New(&user_usecase.Config{
