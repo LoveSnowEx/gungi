264685b80b18269fa399a6d43a56bc587c1e3e87
LoveSnowEx <lsktw22@gmail.com>
2024-09-28 22:47:39 +0800

    Add  GungiController setup

--- internal/bootstrap/api.go
+++ internal/bootstrap/api.go
@@ -5,8 +5,12 @@ import (
 
 	"github.com/gofiber/fiber/v3/middleware/logger"
 
+	"github.com/LoveSnowEx/gungi/internal/app/gungi_usecase"
 	"github.com/LoveSnowEx/gungi/internal/app/user_usecase"
+	"github.com/LoveSnowEx/gungi/internal/domain/gungi_service"
+	"github.com/LoveSnowEx/gungi/internal/infra/notification"
 	"github.com/LoveSnowEx/gungi/internal/infra/persist"
+	"github.com/LoveSnowEx/gungi/internal/interface/api/gungi_controller"
 	"github.com/LoveSnowEx/gungi/internal/interface/api/route"
 	"github.com/LoveSnowEx/gungi/internal/interface/api/user_controller"
 	"github.com/gofiber/fiber/v3"
@@ -29,5 +33,15 @@ func SetupApi(app *fiber.App) {
 				UserRepo: persist.NewUserRepo(),
 			}),
 		}),
+		GungiController: *gungi_controller.New(
+			gungi_usecase.New(
+				&gungi_usecase.GameUsecaseConfig{
+					GameService:  gungi_service.NewGameService(),
+					GameRepo:     persist.NewGameRepo(),
+					PlayerRepo:   persist.NewPlayerRepo(),
+					EventManager: notification.NewGameManager(),
+				},
+			),
+		),
 	})
 }
